694cd0cd93298bb715d2b741d915b6d491e36454
rahul patil <60380578+patil-rahuls@users.noreply.github.com>
2025-09-02 00:36:30 +0530

    Photography Page Fixes

--- config.js
+++ config.js
@@ -16,19 +16,19 @@ var menu = {
   "Contact": "Let's Catch-up!"
 };
 const myPhotos = [
-  { src: 'assets/photography/1.jpeg', w: 531, h: 531 },
-  { src: 'assets/photography/2.jpeg', w: 531, h: 531 },
-  { src: 'assets/photography/3.jpeg', w: 531, h: 531 },
-  { src: 'assets/photography/4.jpeg', w: 531, h: 531 },
-  { src: 'assets/photography/5.jpeg', w: 531, h: 531 },
-  { src: 'assets/photography/6.jpeg', w: 531, h: 531 },
-  { src: 'assets/photography/7.jpeg', w: 531, h: 531 },
-  { src: 'assets/photography/8.jpeg', w: 531, h: 531 },
-  { src: 'assets/photography/9.jpeg', w: 531, h: 531 },
-  { src: 'assets/photography/10.jpeg', w: 531, h: 531 },
-  { src: 'assets/photography/11.jpeg', w: 531, h: 531 },
-  { src: 'assets/photography/12.jpeg', w: 531, h: 531 },
-  { src: 'assets/photography/13.jpeg', w: 531, h: 531 },
-  { src: 'assets/photography/14.jpeg', w: 531, h: 531 },
-  { src: 'assets/photography/15.jpeg', w: 531, h: 531 }
+  { src: '/assets/photography/1.jpeg', width: 531, height: 531 },
+  { src: '/assets/photography/2.jpeg', width: 531, height: 531 },
+  { src: '/assets/photography/3.jpeg', width: 531, height: 531 },
+  { src: '/assets/photography/4.jpeg', width: 531, height: 531 },
+  { src: '/assets/photography/5.jpeg', width: 531, height: 531 },
+  { src: '/assets/photography/6.jpeg', width: 531, height: 531 },
+  { src: '/assets/photography/7.jpeg', width: 531, height: 531 },
+  { src: '/assets/photography/8.jpeg', width: 531, height: 531 },
+  { src: '/assets/photography/9.jpeg', width: 531, height: 531 },
+  { src: '/assets/photography/10.jpeg', width: 531, height: 531 },
+  { src: '/assets/photography/11.jpeg', width: 531, height: 531 },
+  { src: '/assets/photography/12.jpeg', width: 531, height: 531 },
+  { src: '/assets/photography/13.jpeg', width: 531, height: 531 },
+  { src: '/assets/photography/14.jpeg', width: 531, height: 531 },
+  { src: '/assets/photography/15.jpeg', width: 531, height: 531 }
 ];
